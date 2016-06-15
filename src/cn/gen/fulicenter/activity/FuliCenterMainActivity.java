5c9b6ea3f552df0f3bc22cd9e836c234063c63c8
Genxx <1647940270@qq.com>
2016-06-15 17:29:14 +0800

    创建FuliCenterMainActivity,实现闪屏后跳转到这个界面。

---
+++ src/cn/gen/fulicenter/activity/FuliCenterMainActivity.java
@@ -0,0 +1,15 @@
+package cn.gen.fulicenter.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import cn.gen.fulicenter.R;
+
+public class FuliCenterMainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fuli_center_main);
+    }
+}
